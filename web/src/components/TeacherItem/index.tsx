2fb23b4e48fd54f372c4e8d968b59ec67b5547ea
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-08 01:35:41 -0300

    Front-end finalizado

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,35 +2,56 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number
+  avatar: string
+  bio: string
+  cost: number
+  name: string
+  subject: string
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/59893752?s=460&u=c07c8eb71ce76c97bbe756e511597bcb506af578&v=4" alt="Guilherme Illescas" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Guilherme Illescas</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Sou uma pessoa apaixonada por desafios e novos aprendizados! Sempre buscando ser a melhor versão de mim mesmo e melhorar a vida dos outros com o que eu faço!
-        Amo passar tempo imerso em problemas e resolvê-los da melhor forma possível.
-          <br /><br />
-          “Cada segundo é tempo para mudar tudo para sempre”, Charles Chaplin
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
-          Preço/hora
-              <strong>R$ 230,00</strong>
+          Preço/hora:
+              <strong>{teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="WhatsApp" />
               Entrar em contato
-            </button>
+        </a>
       </footer>
     </article>
   )
